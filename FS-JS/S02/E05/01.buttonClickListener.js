330122b3a7172cbe2ada2ed7bdf190fbf542b36b
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-05 21:02:50 +0200

    feat : S02E05 finish

--- FS-JS/S02/E05/01.buttonClickListener.js
+++ FS-JS/S02/E05/01.buttonClickListener.js
@@ -1,2 +1,17 @@
-// Etape 1 - En cas de click sur le bouton défini dans le HTML, 
+// Etape 1 - En cas de click sur le bouton défini dans le HTML,
+function clickButton(target) {
+  const button = document.querySelector("#my-button");
+  console.log(button);
+  button.innerHTML = target;
+  return button;
+}
+
+
+
+document.addEventListener("click", function (event) {
+  event.preventDefault;
+  clickButton(`Bingo !`);
+  
+})
+
 // faire en sorte que le texte de celui-ci change et devienne "Bingo !".
